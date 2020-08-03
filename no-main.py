a202a981f79d2ad2feb88200bce2180fdc03c9cc
alibek <alibek_ch@yahoo.com>
2020-08-03 20:43:16 +0600

    lesson34.proj

---
+++ no-main.py
@@ -0,0 +1,6 @@
+from library1 import *
+
+print(sum(3,4))
+print(sum3(1,2,3))
+print(strlen(a))
+print(strnum(a))
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup 
+
+setup(
+    name="library1",
+    url = "https://github.com/alibekch/PythonLib34.git",
+    author = "alibek",
+    packages = ["library1"],
+    installrequires=[],
+    license="MIT",
+    description = "my own lib"
+)
